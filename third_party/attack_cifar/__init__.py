e0239c75d8fe677622a184322475488ed4a2b687
haichen <54704588+haichen-ber@users.noreply.github.com>
2022-02-25 22:07:45 +0800

    Update __init__.py

--- third_party/attack_cifar/__init__.py
+++ third_party/attack_cifar/__init__.py
@@ -1,7 +1,1 @@
 from third_party.attack_cifar.pgd import PGD
-from third_party.attack_cifar.apgd import APGD, APGDT
-from third_party.attack_cifar.fab import FAB
-from third_party.attack_cifar.square import Square
-from third_party.attack_cifar.multiattack import MultiAttack
-from third_party.attack_cifar.autoattack import autoAttack
-from third_party.attack_cifar.multitarget import multitarget
